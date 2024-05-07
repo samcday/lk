157fe404fcef299df9ee30b22cb8ca7c6f69b84f
Sam Day <me@samcday.com>
2024-05-07 17:34:13 +0200

    fixup! [platform][msm] support platform msm8916
    don't relocate LK on boot, remove unnecessary KERNEL_BASE, add debugcommands + test commands

--- platform/msm/msm8916/rules.mk
+++ platform/msm/msm8916/rules.mk
@@ -4,20 +4,20 @@ MODULE := $(LOCAL_DIR)
 
 ARCH    := arm
 ARM_CPU := cortex-a8
-CPU     := generic
 
 MODULE_SRCS += \
 	$(LOCAL_DIR)/clock.c \
 	$(LOCAL_DIR)/gpio.c \
 	$(LOCAL_DIR)/platform.c
 
-MEMBASE := 0x8F600000
+MEMBASE := 0x80000000
 MEMSIZE := 0x00100000
-KERNEL_BASE = $(MEMBASE)
 
 MODULE_DEPS += \
     dev/interrupt/arm_gic \
-    dev/timer/arm_generic
+    dev/timer/arm_generic \
+    lib/debugcommands \
+    app/tests
 
 include platform/msm/common/rules.mk
 include make/module.mk
